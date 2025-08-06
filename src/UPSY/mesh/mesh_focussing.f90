6ce096ce4814e568b61431fa5d500a891b077116
TijnBerends <c.j.berends@uu.nl>
2025-08-06 15:23:48 +0200

    Add delete_vertices_along_polyline
    Including unit test

---
+++ src/UPSY/mesh/mesh_focussing.f90
@@ -0,0 +1,114 @@
+module mesh_focussing
+
+  use precisions, only: dp
+  use control_resources_and_error_messaging, only: init_routine, finalise_routine
+  use mesh_types, only: type_mesh
+  use polyline_types, only: type_polyline
+  use remapping_types, only: type_single_row_mapping_matrices
+  use line_tracing_Voronoi, only: trace_line_Vor
+  use delete_vertices, only: delete_vertex
+
+  implicit none
+
+  private
+
+  public :: delete_vertices_along_polyline
+
+contains
+
+  subroutine delete_vertices_along_polyline( mesh, ll)
+    !< Delete all vertices whose Voronoi cells are crossed by the polyline
+
+    ! In/output variables:
+    type(type_mesh),     intent(inout) :: mesh
+    type(type_polyline), intent(in   ) :: ll
+
+    ! Local variables:
+    character(len=1024), parameter     :: routine_name = 'delete_vertices_along_polyline'
+    integer, dimension(:), allocatable :: vi_list
+    integer                            :: i, vi_kill, ii
+    integer, dimension(:), allocatable :: vi_new2vi_old, vi_old2vi_new, ti_new2ti_old, ti_old2ti_new
+
+    ! Add routine to path
+    call init_routine( routine_name)
+
+    call list_vertices_crossed_by_polyline( mesh, ll, vi_list)
+
+    do i = 1, size( vi_list)
+
+      ! Delete vertex
+      vi_kill = vi_list( i)
+      call delete_vertex( mesh, vi_kill, vi_new2vi_old, vi_old2vi_new, ti_new2ti_old, ti_old2ti_new)
+
+      ! Update indices of remaining to-be-deleted vertices
+      do ii = i+1, size( vi_list)
+        vi_list( ii) = vi_old2vi_new( vi_list( ii))
+      end do
+
+      ! Clean up after yourself (not sure if memory is automatically deallocated when arrays are allocated in another procedure...)
+      deallocate( vi_new2vi_old)
+      deallocate( vi_old2vi_new)
+      deallocate( ti_new2ti_old)
+      deallocate( ti_old2ti_new)
+
+    end do
+
+    ! Finalise routine path
+    call finalise_routine( routine_name)
+
+  end subroutine delete_vertices_along_polyline
+
+  subroutine list_vertices_crossed_by_polyline( mesh, ll, vi_list)
+    !< List all vertices whose Voronoi cells are crossed by the polyline
+
+    ! In/output variables:
+    type(type_mesh),                    intent(in   ) :: mesh
+    type(type_polyline),                intent(in   ) :: ll
+    integer, dimension(:), allocatable, intent(  out) :: vi_list
+
+    ! Local variables:
+    character(len=1024), parameter         :: routine_name = 'delete_vertices_along_polyline'
+    integer                                :: vi_hint
+    integer                                :: i_end, i1, i2
+    real(dp), dimension(2)                 :: p,q
+    type(type_single_row_mapping_matrices) :: single_row
+
+    ! Add routine to path
+    call init_routine( routine_name)
+
+    if (ll%is_closed) then
+      ! Include the line segment from the last to the first vertex
+      i_end = ll%n
+    else
+      ! Don't
+      i_end = ll%n-1
+    end if
+
+    ! Allocate memory for single row results
+    single_row%n_max = mesh%nV
+    single_row%n     = 0
+    allocate( single_row%index_left( single_row%n_max))
+    allocate( single_row%LI_xdy(     single_row%n_max))
+    allocate( single_row%LI_mxydx(   single_row%n_max))
+    allocate( single_row%LI_xydy(    single_row%n_max))
+
+    vi_hint = 1
+    do i1 = 1, i_end
+      i2 = i1 + 1
+      if (i2 == ll%n+1) i2 = 1
+
+      p = ll%p( i1,:)
+      q = ll%p( i2,:)
+
+      call trace_line_Vor( mesh, p, q, single_row, .true., vi_hint)
+
+    end do
+
+    vi_list = single_row%index_left( 1: single_row%n)
+
+    ! Finalise routine path
+    call finalise_routine( routine_name)
+
+  end subroutine list_vertices_crossed_by_polyline
+
+end module mesh_focussing
--- src/UPSY/validation/unit_tests/ut_mesh_delete_vertices.f90
+++ src/UPSY/validation/unit_tests/ut_mesh_delete_vertices.f90
@@ -13,8 +13,9 @@ module ut_mesh_delete_vertices
   use parameters, only: pi
   use mesh_secondary, only: calc_all_secondary_mesh_data
   use delete_vertices, only: delete_vertex
-
-  use netcdf_io_main
+  use polyline_types, only: type_polyline
+  use mesh_memory, only: duplicate_mesh_primary
+  use mesh_focussing, only: delete_vertices_along_polyline
 
   implicit none
 
@@ -41,7 +42,8 @@ contains
     ! Add test name to list
     test_name = trim( test_name_parent) // '/' // trim( test_name_local)
 
-    call test_delete_single_vertex( test_name)
+    call test_delete_single_vertex          ( test_name)
+    call test_delete_vertices_along_polyline( test_name)
 
     ! Remove routine from call stack
     call finalise_routine( routine_name)
@@ -49,7 +51,7 @@ contains
   end subroutine test_delete_vertices
 
   subroutine test_delete_single_vertex( test_name_parent)
-    ! Test the trace_line_grid_a subroutine
+    ! Test the delete_vertex subroutine
 
     ! In/output variables:
     character(len=*), intent(in) :: test_name_parent
@@ -118,5 +120,72 @@ contains
     call finalise_routine( routine_name)
 
   end subroutine test_delete_single_vertex
+
+  subroutine test_delete_vertices_along_polyline( test_name_parent)
+    ! Test the delete_vertices_along_polyline subroutine
+
+    ! In/output variables:
+    character(len=*), intent(in) :: test_name_parent
+
+    ! Local variables:
+    character(len=1024), parameter     :: routine_name = 'test_delete_vertices_along_polyline'
+    character(len=1024), parameter     :: test_name_local = 'delete_vertices_along_polyline'
+    character(len=1024)                :: test_name
+    real(dp), parameter                :: xmin = -1._dp
+    real(dp), parameter                :: xmax =  1._dp
+    real(dp), parameter                :: ymin = -1._dp
+    real(dp), parameter                :: ymax =  1._dp
+    real(dp)                           :: alpha_min
+    real(dp)                           :: res_max
+    type(type_mesh)                    :: mesh, mesh2
+    type(type_polyline)                :: ll
+    integer                            :: i
+    real(dp)                           :: r, theta, x, y
+    integer                            :: nV_before
+
+    ! Add routine to call stack
+    call init_routine( routine_name)
+
+    ! Add test name to list
+    test_name = trim( test_name_parent) // '/' // trim( test_name_local)
+
+    call allocate_mesh_primary( mesh, 'test_mesh', 100, 200)
+    call initialise_dummy_mesh_5( mesh, xmin, xmax, ymin, ymax)
+
+    ! Refine the test mesh
+    alpha_min = 25._dp * pi / 180._dp
+    res_max = pi / 50._dp
+    call refine_mesh_uniform( mesh, res_max, alpha_min)
+    call crop_mesh_primary( mesh)
+    call calc_all_secondary_mesh_data( mesh , 0._dp, -90._dp, 71._dp)
+
+    ! Define a nice circular polyline
+    ll%is_closed = .true.
+    ll%n         = 100
+    allocate( ll%p( ll%n,2), source = 0._dp)
+
+    r = mesh%xmax * 0.6_dp
+    do i = 1, ll%n
+      theta = real( i,dp) / real( ll%n,dp) * 2._dp * pi
+      x = r * cos( theta)
+      y = r * sin( theta)
+      ll%p( i,:) = [x,y]
+    end do
+
+    ! Duplicate the mesh
+    call duplicate_mesh_primary( mesh, mesh2)
+    call calc_all_secondary_mesh_data( mesh2, 0._dp, -90._dp, 71._dp)
+
+    ! Delete vertices along the polyline
+    call delete_vertices_along_polyline( mesh2, ll)
+    call calc_all_secondary_mesh_data( mesh2, 0._dp, -90._dp, 71._dp)
+
+    call unit_test( test_mesh_is_self_consistent( mesh2), trim( test_name) // '/mesh_self_consistency')
+    call unit_test( mesh2%nV < mesh%nV, trim( test_name) // '/nV')
+
+    ! Remove routine from call stack
+    call finalise_routine( routine_name)
+
+  end subroutine test_delete_vertices_along_polyline
 
 end module ut_mesh_delete_vertices
