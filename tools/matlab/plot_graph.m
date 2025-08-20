7c0f85193b81aa9c8df03918823f921d62019b88
TijnBerends <c.j.berends@uu.nl>
2025-08-20 10:24:07 +0200

    Add plot_graph

---
+++ tools/matlab/plot_graph.m
@@ -0,0 +1,24 @@
+function H = plot_graph( graph)
+
+% Axes and figure size
+xw = graph.xmax - graph.xmin;
+yw = graph.ymax - graph.ymin;
+if xw >= yw
+  wa = 800;
+  ha = wa * yw / xw;
+else
+  ha = 800;
+  wa = ha * xw / yw;
+end
+wf = 25 + wa + 25;
+hf = 25 + ha + 25;
+
+H.Fig = figure('position',[200,200,wf,hf],'color','w');
+H.Ax  = axes('parent',H.Fig,'units','pixels','position',[25,25,wa,ha],'fontsize',24,...
+  'xtick',[],'ytick',[],'xlim',[graph.xmin, graph.xmax],'ylim',[graph.ymin, graph.ymax]);
+
+H.Graph = add_graph_to_plot( H.Ax, graph);
+
+set( H.Ax,'units','normalized');
+
+end
