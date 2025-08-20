bd18395b0d3a7aa1e10726bccc9a03c2571503e9
Erwin Lambert <45197956+erwinlambert@users.noreply.github.com>
2025-08-20 15:02:34 +0200

    clean up BMB colormaps

--- tools/python/colormaps.py
+++ tools/python/colormaps.py
@@ -35,81 +35,53 @@ def get_cmap(varname):
 
         cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', colors)
         norm = mpl.colors.SymLogNorm(linthresh, vmin=vmin, vmax=vmax, linscale=linscale)
-        
+
     elif varname == 'BMB_v3':
         #Create BMB colormap
+        Ncols = 68
         vmax = 200
         vmin = -10
-        vdark = 10
         linthresh = .3
         linscale = .25
         fracpos = (np.log10(vmax/linthresh)+linscale)/(np.log10(vmax/linthresh)+np.log10(-(vmin/linthresh))+2*linscale)
-        nneg = np.int_((1-fracpos)*256) + 1
-        fraclopos = (np.log10(vdark/linthresh)+linscale)/(np.log10(vmax/linthresh)+linscale)
-        npos = 256-nneg
-        nlopos = np.int_(fraclopos*npos) + 1
-        colors1 = plt.get_cmap('cmo.diff')(np.linspace(0,.48,nneg))
-        colors2 = plt.get_cmap('cmo.curl')(np.linspace(.5,.91,nlopos))
-        colors3 = plt.get_cmap('inferno')(np.linspace(.3,1,256-nneg-nlopos))
+        nneg = np.int_((1-fracpos)*Ncols) + 1
+        colors1 = plt.get_cmap('cmo.diff')(np.linspace(.15,.5,nneg))
+        colors2 = plt.get_cmap('afmhot_r')(np.linspace(0,.9, Ncols-nneg))
+        colors = np.vstack((colors1, colors2))
 
-        colors = np.vstack((colors1, colors2, colors3))
-
-        cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', colors)
+        cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', colors, Ncols)
         norm = mpl.colors.SymLogNorm(linthresh, vmin=vmin, vmax=vmax, linscale=linscale)
 
     elif varname == 'BMB_v4':
-        #Create BMB colormap
-        Ncols = 68#256
         vmax = 200
         vmin = -10
-        vdark = 10
+        vlim = 30
         linthresh = .3
         linscale = .25
         fracpos = (np.log10(vmax/linthresh)+linscale)/(np.log10(vmax/linthresh)+np.log10(-(vmin/linthresh))+2*linscale)
-        nneg = np.int_((1-fracpos)*Ncols) + 1
-        fraclopos = (np.log10(vdark/linthresh)+linscale)/(np.log10(vmax/linthresh)+linscale)
-        npos = Ncols-nneg
-        nlopos = np.int_(fraclopos*npos) + 1
-        colors1 = plt.get_cmap('cmo.diff')(np.linspace(0,.46,nneg))
-        colors2 = plt.get_cmap('cmo.amp')(np.linspace(.02,1,nlopos))
-        colors3 = plt.get_cmap('cmo.solar')(np.linspace(0.02,1,Ncols-nneg-nlopos))
-
+        nneg = np.int_((1-fracpos)*256) +1
+        npos = 256-nneg
+        fraclopos = (np.log10(vlim/linthresh)+linscale)/(np.log10(vmax/linthresh)+linscale)
+        nlopos = np.int_(fraclopos*npos) +1
+        
+        colors1 = plt.get_cmap('bone')(np.linspace(0,1.,nneg))
+        colors2 = plt.get_cmap('afmhot_r')(np.linspace(0., .95, nlopos))
+        colors3 = plt.get_cmap('magma')(np.linspace(0.05, .4, 256-nneg-nlopos))
         colors = np.vstack((colors1, colors2, colors3))
-
-        cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', colors, Ncols)
+        
+        cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', colors,68)
         norm = mpl.colors.SymLogNorm(linthresh, vmin=vmin, vmax=vmax, linscale=linscale)
-
-    elif varname == 'BMB_v5':
-        #Create BMB colormap
-        Ncols = 68#256
-        vmax = 200
-        vmin = -10
-        vdark = 10
-        linthresh = .3
-        linscale = .25
-        fracpos = (np.log10(vmax/linthresh)+linscale)/(np.log10(vmax/linthresh)+np.log10(-(vmin/linthresh))+2*linscale)
-        nneg = np.int_((1-fracpos)*Ncols) + 1
-        fraclopos = (np.log10(vdark/linthresh)+linscale)/(np.log10(vmax/linthresh)+linscale)
-        npos = Ncols-nneg
-        nlopos = np.int_(fraclopos*npos) + 1
-        colors1 = plt.get_cmap('cmo.diff')(np.linspace(0,.46,nneg))
-        colors2 = plt.get_cmap('gist_heat_r')(np.linspace(.02,1,nlopos))
-        colors3 = plt.get_cmap('inferno')(np.linspace(0,1,Ncols-nneg-nlopos))
-
-        colors = np.vstack((colors1, colors2, colors3))
-
-        cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', colors, Ncols)
-        norm = mpl.colors.SymLogNorm(linthresh, vmin=vmin, vmax=vmax, linscale=linscale)
-
+        
     elif varname == 'Hi':
         cmap = copy(plt.get_cmap('cmo.ice'))
-        norm = mpl.colors.Normalize(vmin=0,vmax=4000,clip=True)
+        norm = mpl.colors.Normalize(vmin=0,vmax=3000,clip=True)
 
     elif varname == 'dHi':
         colors1 = plt.get_cmap('afmhot')(np.linspace(0,1,128))
         colors2 = plt.get_cmap('bone_r')(np.linspace(0,1,128))
         colors = np.vstack((colors1,colors2))
         cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', colors, 256)
+        cmap = copy(plt.get_cmap('cmo.tarn'))
         norm = mpl.colors.Normalize(vmin=-1000,vmax=1000,clip=True)
 
     elif varname == 'Hib':
@@ -148,7 +120,7 @@ def get_cmap(varname):
         cmap = copy(plt.get_cmap('Greens'))
         norm = mpl.colors.Normalize(vmin=0,vmax=1.0,clip=True)
 
-    elif varname in ['uabs_surf', 'uabs_vav']:
+    elif varname in ['uabs_surf', 'uabs_vav', 'bed_roughness_nudge_H_u_target_velocity']:
         cmap = copy(plt.get_cmap('turbo'))
         norm = mpl.colors.LogNorm(vmin=1.,vmax=4000,clip=True)
 
