65926221d34a7cde1f9062ef9a589101896c08f2
TijnBerends <c.j.berends@uu.nl>
2026-01-04 09:22:27 +0100

    Add some more fields to demo model
    Hurray

--- src/UPSY/models/models_demo.f90
+++ src/UPSY/models/models_demo.f90
@@ -20,8 +20,10 @@ module models_demo
     ! Some ice-model-esque data fields
     real(dp), dimension(:  ), contiguous, pointer :: H
     real(dp), dimension(:,:), contiguous, pointer :: u_3D
+    real(dp), dimension(:,:), contiguous, pointer :: v_3D
     logical,  dimension(:  ), contiguous, pointer :: mask_ice
-    type(MPI_WIN) :: wH, wu_3D, wmask_ice
+    real(dp), dimension(:,:), contiguous, pointer :: T2m
+    type(MPI_WIN) :: wH, wu_3D, wv_3D, wmask_ice, wT2m
 
   contains
 
@@ -59,9 +61,15 @@ contains
       units     = 'm')
 
     call model%flds_reg%create_field( model%u_3D, model%wu_3D, &
-      mesh, Arakawa_grid%b(), third_dimension%ice_zeta( nz), &
+      mesh, Arakawa_grid%b(), third_dimension%ice_zeta( nz, 'regular'), &
       name      = 'u_3D', &
       long_name = 'depth-dependent horizontal ice velocity in x-direction', &
+      units     = 'm yr^-1')
+
+    call model%flds_reg%create_field( model%v_3D, model%wv_3D, &
+      mesh, Arakawa_grid%b(), third_dimension%ice_zeta( nz, 'regular'), &
+      name      = 'v_3D', &
+      long_name = 'depth-dependent horizontal ice velocity in y-direction', &
       units     = 'm yr^-1')
 
     call model%flds_reg%create_field( model%mask_ice, model%wmask_ice, &
@@ -69,6 +77,12 @@ contains
       name      = 'mask_ice', &
       long_name = 'ice mask', &
       units     = '-')
+
+    call model%flds_reg%create_field( model%T2m, model%wT2m, &
+      mesh, Arakawa_grid%a(), third_dimension%month(), &
+      name      = 'T2m', &
+      long_name = 'Monthly 2-m air temperature', &
+      units     = 'K')
 
     ! Initialise fields with simple analytical functions
 
