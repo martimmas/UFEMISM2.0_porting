bb9f77dc93f5f571fb041af9296a9eff97d404ea
TijnBerends <c.j.berends@uu.nl>
2026-01-07 15:07:07 +0100

    Check third dimension
    Hurray

--- src/UPSY/fields/fields_basic_submod_read_from_netcdf.f90
+++ src/UPSY/fields/fields_basic_submod_read_from_netcdf.f90
@@ -19,6 +19,17 @@ contains
     ! Add routine to call stack
     call init_routine( routine_name)
 
+    select type (f => field)
+    class default
+      call crash('invalid field type')
+    class is (atype_field_2D)
+    class is (atype_field_3D)
+      if (.not. f%third_dimension_val%is_dim_and_var_in_netcdf( filename, ncid)) then
+        call crash('third dimension of field "' // trim( field%name()) // &
+          '" not found in file "' // trim( filename) // '"')
+      end if
+    end select
+
     call crash('whoopsiedaisy')
 
     ! Remove routine from call stack
