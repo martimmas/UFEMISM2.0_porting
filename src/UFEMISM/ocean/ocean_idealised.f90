d9a5d755da442766102891e000d4f4716b5854af
FrankaJes <61973646+FrankaJes@users.noreply.github.com>
2026-01-09 17:22:34 +0100

    Added flexible config variables to determine forcing

--- src/UFEMISM/ocean/ocean_idealised.f90
+++ src/UFEMISM/ocean/ocean_idealised.f90
@@ -230,10 +230,7 @@ CONTAINS
   ! ============
 
   SUBROUTINE initialise_ocean_model_idealised_LINEAR_THERMOCLINE( mesh, ocean)
-    ! Idealised forcing to compare with the MITgcm runs done by Paul for the Amundsen Sea
-    ! From 0 - 300 m depth: 34 PSU, -1.0 degC --> 200 m for THW 200m runs, 300 m for AMU 400m runs
-    ! From 700 - deep: 34.7 PSU, 1.2 degC --> 600 m for THW 200m runs, 700 m for AMU 400m runs
-    ! In between these layers: a linear increase in temperature and salinity
+    ! Idealised forcing representing a two-layer ocean forcing separated by a linear thermocline in between
 
     IMPLICIT NONE
 
@@ -244,31 +241,31 @@ CONTAINS
     CHARACTER(LEN=256), PARAMETER                       :: routine_name = 'initialise_ocean_model_idealised_LINEAR_THERMOCLINE'
     INTEGER                                             :: vi
     INTEGER                                             :: k
-    REAL(dp), PARAMETER                                 :: S0 = 34.0_dp    ! [PSU]  Surface salinity
-    REAL(dp), PARAMETER                                 :: T0 = -1.0_dp    ! [deg C]  Surface temperature
-    REAL(dp), PARAMETER                                 :: S1 = 34.7_dp    ! [PSU]  Surface salinity
-    REAL(dp), PARAMETER                                 :: T1 = 1.2_dp     ! [deg C]  Surface temperature
-    REAL(dp), PARAMETER                                 :: thermo_depth_start = 200.0_dp ! depth at which thermocline starts
-    REAL(dp), PARAMETER                                 :: thermo_depth_end = 600.0_dp ! depth at which thermocline starts
+    REAL(dp)                                            :: S0, S1, T0, T1
 
     ! Add routine to path
     CALL init_routine( routine_name) 
+
+    S0 = C%ocean_lin_therm_surf_salinity
+    S1 = C%ocean_lin_therm_deep_salinity
+    T0 = C%ocean_lin_therm_surf_temperature
+    T1 = C%ocean_lin_therm_deep_temperature
 
     DO vi = mesh%vi1, mesh%vi2
       DO k = 1, C%nz_ocean
 
         ! Surface layer
-        IF (C%z_ocean( k) <= thermo_depth_start ) THEN 
+        IF (C%z_ocean( k) <= C%ocean_lin_therm_thermocline_top ) THEN 
           ocean%T( vi, k) = T0
           ocean%S( vi, k) = S0
 
         ! Intermediate layer (thermocline), linearly increase temperature and salinity
-        ELSEIF (C%z_ocean( k) > thermo_depth_start  .AND. C%z_ocean( k) < thermo_depth_end) THEN 
-          ocean%T( vi, k) = T0 + (T1-T0)*(C%z_ocean( k)-thermo_depth_start)/(thermo_depth_end-thermo_depth_start)
-          ocean%S( vi, k) = S0 + (S1-S0)*(C%z_ocean( k)-thermo_depth_start)/(thermo_depth_end-thermo_depth_start)
+        ELSEIF (C%z_ocean( k) > C%ocean_lin_therm_thermocline_top  .AND. C%z_ocean( k) < C%ocean_lin_therm_thermocline_bottom) THEN 
+          ocean%T( vi, k) = T0 + (T1-T0)*(C%z_ocean( k)-C%ocean_lin_therm_thermocline_top)/(C%ocean_lin_therm_thermocline_bottom-C%ocean_lin_therm_thermocline_top)
+          ocean%S( vi, k) = S0 + (S1-S0)*(C%z_ocean( k)-C%ocean_lin_therm_thermocline_top)/(C%ocean_lin_therm_thermocline_bottom-C%ocean_lin_therm_thermocline_top)
 
         ! Deep layer
-        ELSEIF (C%z_ocean( k) >= thermo_depth_end) THEN 
+        ELSEIF (C%z_ocean( k) >= C%ocean_lin_therm_thermocline_bottom) THEN 
           ocean%T( vi, k) = T1
           ocean%S( vi, k) = S1
 
