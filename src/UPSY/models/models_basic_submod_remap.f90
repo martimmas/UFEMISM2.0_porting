ae80f889092a213801d0c195ed350386eaa63a9d
TijnBerends <c.j.berends@uu.nl>
2026-01-16 20:05:18 +0100

    Add set_bounds to model remap
    Hurray

--- src/UPSY/models/models_basic_submod_remap.f90
+++ src/UPSY/models/models_basic_submod_remap.f90
@@ -15,14 +15,21 @@ contains
     ! Add routine to call stack
     call init_routine( routine_name)
 
+    ! Safety: check that this model is indeed defined on a mesh
     select type (g => self%grid())
     class default
       call crash('remapping only defined for mesh-based models')
     class is (type_mesh)
     end select
 
+    ! Remap all the individual fields
     call self%flds_reg%remap( mesh_new)
+
+    ! Set the model mesh to the new one
     call self%set_grid( mesh_new)
+
+    ! Update the array bounds for all the actual fields
+    call self%set_bounds
 
     ! Remove routine from call stack
     call finalise_routine( routine_name)
