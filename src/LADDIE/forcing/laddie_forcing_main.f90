81ae2cee5b975f056a14f1ec72999f8627719337
erwinlambert <erwin.lambert@knmi.nl>
2026-03-04 13:21:20 +0100

    add write statement

--- src/LADDIE/forcing/laddie_forcing_main.f90
+++ src/LADDIE/forcing/laddie_forcing_main.f90
@@ -326,6 +326,9 @@ contains
     ! Add routine to path
     call init_routine( routine_name)
 
+    ! Print to screen
+    if (par%primary) write(0,'(A)') '   Creating mesh from referenge geometry...'
+
     ! Determine if the initial geometry is provided gridded or meshed
     if (allocated( refgeo%grid_raw%x)) then
       ! Gridded
