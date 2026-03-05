80ddd5c65e9fb88d3519201f9e62c1fc901d7b18
erwinlambert <erwin.lambert@knmi.nl>
2026-03-05 11:00:45 +0100

    add bunch of comments to figure

--- tools/python/upsy/figure.py
+++ tools/python/upsy/figure.py
@@ -24,6 +24,8 @@ class Figure(object):
         shrink: list = [0,0,0,0],
     ):
 
+        """ Some input parameters with default settings """
+
         self.figsize = figsize
         self.directory = directory
         self.orientation = orientation
@@ -41,8 +43,9 @@ class Figure(object):
     def __str__(self):
         return f"Figure"
 
-
     def add_field(self, tf, varname, mask=None):
+        """Add a field (say BMB of the last time frame) to the figure"""
+
         field = Field(tf, varname, mask=mask)
 
         print(f'Added {varname} with hash {hash(field)}')
@@ -51,6 +54,11 @@ class Figure(object):
         return 
 
     def add_diff(self, Timeframe1, varname1, Timeframe2, varname2, name='diff', mask=None, vmax=None, colmap='cmo.diff'):
+        """ Add a difference field to the figure, 
+        for example the difference of 1 variable between 2 timeframes
+        or the difference of two variables on the same timeframe
+        """
+
         field1 = Field(Timeframe1, varname1)
         field2 = Field(Timeframe2, varname2)
         field = DiffField(field1, field2, name=name, mask=mask, vmax=vmax, colmap=colmap)
@@ -61,20 +69,27 @@ class Figure(object):
         return 
 
     def delete_field(self,fieldhash):
+        # Delete a field from the figure
 
         del self.fields[fieldhash]
+
         return
 
     def print_fields(self):
+        """Print all fields added to the figure thus far"""
 
         print(self.fields.keys())
+
         return
 
     def make(self,figname, add_gl=True, add_time=False):
+        """Make the figure with all the added fields """
 
         fig = plt.figure(figsize=self.figsize,constrained_layout=True)
 
+        # Design the layout of the panels
         if self.orientation=='horizontal':
+            # Align all panels horizontally, add a row below for the colorbar
             nrows = 2
             ncols = len(self.fields)
             spec = gridspec.GridSpec(nrows,ncols,figure=fig,height_ratios=[self.cbar_ratio,1],hspace=0.01,wspace=0.01)
@@ -82,6 +97,7 @@ class Figure(object):
                 self.fields[key].ax = fig.add_subplot(spec[0,k])
                 self.fields[key].cax = fig.add_subplot(spec[1,k])
         else:
+            # Align all panels vertically, add a row to the right for the colorbar
             nrows = len(self.fields)
             ncols = 2
             spec = gridspec.GridSpec(nrows,ncols,figure=fig,width_ratios=[self.cbar_ratio,1],hspace=0.01,wspace=0.01)
@@ -89,21 +105,27 @@ class Figure(object):
                 self.fields[key].ax = fig.add_subplot(spec[k,0])
                 self.fields[key].cax = fig.add_subplot(spec[k,1])
 
+        # Plot fields on subpanels
         for k,key in enumerate(self.fields.keys()):
             field = self.fields[key]
             if field.mask is not None:
+                # Add mask of, say, grounded ice/ocean
                 field.get_mcoll()
                 field.ax.add_collection(field.mcoll)
+            # Add field itself
             field.get_pcoll()
             field.ax.add_collection(field.pcoll)
+            # Add colorbar
             cbar = plt.colorbar(field.scalarmap,cax=field.cax,orientation=self.orientation)
             cbar.set_label(field.varname)
 
             if add_gl:
+                # Add the grounding line with some default settings
                 if not field.Timeframe.got_gl:
                     field.Timeframe.get_gl()
                 field.ax.plot(field.Timeframe.gl[0,:],field.Timeframe.gl[1,:],c='k',lw=.5)
 
+            # Shrink each subpanel as requested
             field.ax.set_xlim([field.xmin+self.shrink[0], field.xmax-self.shrink[1]])
             field.ax.set_ylim([field.ymin+self.shrink[2], field.ymax-self.shrink[3]])
             field.ax.set_aspect(1)
@@ -111,8 +133,10 @@ class Figure(object):
             field.ax.set_yticks([])
 
         if add_time:
+            # Add the timestamp, mostly useful for movies
             fig.suptitle(f'Year {field.Timeframe.time:.0f}')
 
+        # Save figure in desired directory
         fullfigname = os.path.join(self.directory,f'{figname}.png')
         plt.savefig(fullfigname, bbox_inches = 'tight', pad_inches = 0,dpi=self.dpi)
         print(f'Created {figname}')
@@ -121,17 +145,21 @@ class Figure(object):
         return
 
 def make_2dplot(
-    rundir: str,
-    variables: list | str,
-    timeindices: list | int,
-    figsize: str | tuple = (7,5),
-    orientation: str = 'horizontal',
-    cbar_ratio: int | float = 30,
-    directory: str = 'figures',
-    shrink: str | list = [0,0,0,0],
-    dpi: str | int = 450,
+    rundir: str, #Directory of the run where output is stored
+    variables: list | str, #Variables to plot
+    timeindices: list | int, #Time indices (integers) to plot
+    figsize: str | tuple = (7,5), #Specify the figure size: (width,height)
+    orientation: str = 'horizontal', #Alignment of panels
+    cbar_ratio: int | float = 30, #Relative width/height of the colorbar
+    directory: str = 'figures', #Directory to plot figure
+    shrink: str | list = [0,0,0,0], #Shrink the domain size of each panel
+    dpi: str | int = 450, #DPI of the output figure
 ):
 
+    """ Function to make the actual plot,
+    allowing user input """
+
+    # Change input types to required ones
     try:
         figsize = tuple(figsize)
         shrink = list(shrink)
@@ -142,6 +170,7 @@ def make_2dplot(
     except ValueError:
         raise argparse.ArgumentTypeError("Invalid input type")
 
+    # Check whether input is valid: either 1 variable, 1 timeindex, or the same number of both
     if (len(variables) > 1 and len(timeindices) > 1):
         assert len(variables) == len(timeindices), f'Number of variables ({len(variables)}) must be equal to number of timeindices ({len(timeindices)})'
     elif len(variables) > 1:
@@ -149,8 +178,10 @@ def make_2dplot(
     elif len(timeindices) > 1:
         variables = variables*len(timeindices)
 
+    # Make directory
     os.makedirs(directory,exist_ok=True)
 
+    # Prepare figure
     fig = Figure(
         figsize=figsize,
         directory=directory,
@@ -160,21 +191,37 @@ def make_2dplot(
         shrink=shrink,
     )
 
+    # Open run
     run = Run(rundir)
 
+    # Define beginning of figure name
     figname = f'{os.path.basename(rundir)}_'
+
+    # Add fields for desired variables and timeindices
     for tidx,varname in zip(timeindices,variables):
 
-        #TODO find mesh number
+        #TODO find mesh number. For now, only works on the first one
         midx = 1
         mesh = Mesh(run,midx)
         tf = Timeframe(mesh, tidx)
         fig.add_field(tf=tf, varname=varname, mask=None)
         figname += f'{varname}_{tidx}_'
 
+    # Make the actual figure, leave out the last underscore
     fig.make(figname[:-1])
 
+
 def main():
+    """ This is the command-line interface to plot a 2D figure
+    Example usage:
+
+    uspy-plot-2dfigure rundir (default: show BMB on last timeframe)
+    uspy-plot-2dfigure rundir -v R BMB_v3 (show two variables: resolution and BMB)
+    uspy-plot-2dfigure rundir -t 0 -1 (show two timeframes of BMB)
+
+    """
+
+    # Parse command-line input
     parser = argparse.ArgumentParser(
     description='Make figure of subplots'
     )
@@ -260,11 +307,11 @@ def main():
 
     args = parser.parse_args()
 
+    # Set default location to rundir/figures
     if args.directory == '':
         args.directory = os.path.join(args.rundir,'figures')
 
-    print(args.directory)
-
+    # Make the plot
     make_2dplot(
         rundir = args.rundir,
         variables = args.variables,
@@ -282,7 +329,13 @@ class Field(object):
     """ Field to include in subplot """
 
     def __init__(self, Timeframe, varname, mask=None):
+        """ Input:
+        Timeframe: class(Timeframe)
+        varname: str
+        mask: desired mask, default: None
+        """
 
+        # Extract info from input
         self.Timeframe = Timeframe
         self.Mesh = self.Timeframe.Mesh
         self.t = self.Timeframe.t
@@ -295,8 +348,11 @@ class Field(object):
         self.ymin = self.Timeframe.ds.ymin
         self.ymax = self.Timeframe.ds.ymax
 
+        # Define the colormap based on the variable name
+        # These are pre-defined but can be overwritten
         self.get_cmap()
 
+        # Read the field data
         self.get_data()
 
     def __repr__(self):
@@ -308,12 +364,14 @@ class Field(object):
     def get_data(self):
         """ Extract data values """
 
-
+        # Special cases
         if self.varname == 'Uabs_lad':
+            # Absolute speed, based on U and V velocity
             uvar = self.Timeframe.ds['U_lad']
             vvar = self.Timeframe.ds['V_lad']
             self.data = (uvar**2+vvar**2)**.5
         elif self.varname[:3] == 'BMB':
+            # BMB or melt, with different versions
             if 'BMB' in self.Timeframe.ds:
                 self.data = -self.Timeframe.ds['BMB']
             elif 'melt' in self.Timeframe.ds:
@@ -322,6 +380,7 @@ class Field(object):
                 print(f"ERROR: no valid BMB or melt variable in Timeframe")
                 return
         else:
+            # Regular case: read variable if available in output
             try:
                 self.data = self.Timeframe.ds[self.varname]
             except KeyError:
@@ -329,10 +388,14 @@ class Field(object):
                 return
 
         if self.mask is not None:
+            # Read mask, and mask out data by setting to NaN
             if 'vi' in self.data.dims:
+
+                # Read mask values
                 if not self.Timeframe.got_mask:
                     self.Timeframe.get_mask()
 
+                # Mask out data in desired regions
                 if self.mask == 'shelf':
                     self.data = xr.where([x in [4] for x in self.Timeframe.mask], self.data, np.nan)
                 elif self.mask == 'sheet':
@@ -341,8 +404,11 @@ class Field(object):
                     self.data = xr.where([x in [3,4] for x in self.Timeframe.mask], self.data, np.nan)
                 else:
                     raise ValueError('Invalid option for mask')
+
             elif 'ti' in self.data.dims:
-                # Just mask out zero values, no clean option yet
+                # For data on triangles, no clean option yet
+                # Just mask out region where data values are zero
+
                 self.data = xr.where(self.data==0, np.nan, self.data)
             else:
                 print(f'ERROR: variable {varname} is not on vertices or triangles')
@@ -396,12 +462,17 @@ class Field(object):
         return
 
 class DiffField(object):
-    """ Difference field between two Fields """
+    """ Difference field between two Fields 
+
+        Diff == Field1 - Field2
+    """
 
     def __init__(self, Field1, Field2, name, mask, vmax, colmap):
 
+        # Read two fields
         self.Field1 = Field1
         self.Field2 = Field2
+        # Read the timeframe of the first field, for example to plot the grounding line
         self.Timeframe = self.Field1.Timeframe
 
         self.name = name
@@ -413,9 +484,11 @@ class DiffField(object):
         self.ymin = self.Field1.ymin
         self.ymax = self.Field1.ymax
 
+        # Define the colormap and range (assumed to be symmetric: -vmax to vmax)
         self.vmax = vmax
         self.colmap = colmap
 
+        # Check whether the two fields are compatible (on the same mesh, both voronoi or triangle)
         self.check_compatibility()
 
         self.get_cmap()
@@ -431,11 +504,13 @@ class DiffField(object):
     def check_compatibility(self):
         """ Check whether difference field can be computed """
 
+        # Check whether the domain is equal
         assert self.xmin == self.Field2.xmin, 'ERROR: xmin is not equal between input Fields'
         assert self.xmax == self.Field2.xmax, 'ERROR: xmax is not equal between input Fields'
         assert self.ymin == self.Field2.ymin, 'ERROR: ymin is not equal between input Fields'
         assert self.ymax == self.Field2.ymax, 'ERROR: ymax is not equal between input Fields'
 
+        # Check whether the dimensions (vi or ti) are equal
         if 'vi' in self.Field1.data.dims:
             assert 'vi' in self.Field2.data.dims, 'ERROR: cannot take difference between voronois and triangles'
             assert (self.Field1.Timeframe.ds['V'] == self.Field2.Timeframe.ds['V']).all(), 'ERROR: V is not equal between input Fields'
@@ -444,11 +519,15 @@ class DiffField(object):
             assert (self.Field1.Timeframe.ds['Tri'] == self.Field2.Timeframe.ds['Tri']).all(), 'ERROR: ti is not equal between input Fields'
 
         print('Input Fields are compatible')
+
+        # Inherit the mesh
         self.Mesh = self.Field1.Timeframe.Mesh
+
         return
 
     def get_cmap(self):
         """ Define cmap and norm """
+
         self.cmap = plt.get_cmap(self.colmap)
 
         if self.vmax is None:
